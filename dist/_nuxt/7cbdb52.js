2a05a54461a438975d0a4c7dbe76887506adb21c
Sebastian hilton <bastianh86@outlook.com>
2021-12-31 23:58:13 -0800

    updated javascript, removed footer links, removed client-only tag from index

--- dist/_nuxt/7cbdb52.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{260:function(e,n,t){"use strict";t.r(n);var c={name:"IndexPage"},l=t(31),component=Object(l.a)(c,(function(){var e=this.$createElement,n=this._self._c||e;return n("div",[n("client-only",[n("div",{staticClass:"calendarscreen",attrs:{id:"datepicker"}})])],1)}),[],!1,null,null,null);n.default=component.exports}}]);
